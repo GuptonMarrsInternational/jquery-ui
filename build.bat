829bd59c99e24669966ee7cf47492de0d72f81ec
Rand Scullard <rand@randscullard.com>
2015-10-27 17:33:48 -0400

    guptonMarrs: Add build.bat for convenience.

---
+++ build.bat
@@ -0,0 +1,3 @@
+call grunt sizer
+copy /y dist\jquery-ui.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQuery\jquery-ui.js
+copy /y dist\jquery-ui.min.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQuery\jquery-ui.prod.js
