615641ac240c18052d04a26413532e20872a9543
Rand Scullard <rand@randscullard.com>
2016-10-20 15:34:00 -0400

    guptonMarrs: build.bat - copy output files to jQueryUI folder

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call grunt sizer
-copy /y dist\jquery-ui.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQuery\jquery-ui.js
-copy /y dist\jquery-ui.min.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQuery\jquery-ui.prod.js
+copy /y dist\jquery-ui.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQueryUI\jquery-ui.js
+copy /y dist\jquery-ui.min.js \GMI\GMISoftware\FIRMV2\FIRMWebSite\ThirdParty\jQueryUI\jquery-ui.prod.js
